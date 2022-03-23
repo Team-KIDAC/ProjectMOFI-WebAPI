c023a68149e118f686cf1272e31a5a52aa79d47e
kalrit718 <kalindu.rithmal.other@gmail.com>
2022-03-23 14:05:20 +0530

    Attendee model and controller added.

---
+++ Controllers/AttendeeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectMOFI_Server_WebAPI.Controllers {
+    [Route("[controller]")]
+    [ApiController]
+    public class AttendeeController : ControllerBase {
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsers() {
+            try {
+                return await Task.FromResult(Ok("Hooray!"));
+            }
+            catch (Exception ex) {
+                Console.WriteLine(ex.StackTrace);
+                return StatusCode(500);
+            }
+        }
+    }
+}
---
+++ Models/Attendee.cs
@@ -0,0 +1,8 @@
+﻿namespace ProjectMOFI_Server_WebAPI.Models {
+    public class Attendee {
+        public string Id { get; init; }
+        public string Name { get; init; }
+        public string Department { get; init; }
+        public string Vaccine { get; init; }
+    }
+}
