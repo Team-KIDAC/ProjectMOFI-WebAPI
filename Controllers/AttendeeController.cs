0dce806129ae662b0f0d9585e406ffde0acee10e
kalrit718 <kalindu.rithmal.other@gmail.com>
2022-03-23 18:18:16 +0530

    Added GET Attendees method.

--- Controllers/AttendeeController.cs
+++ Controllers/AttendeeController.cs
@@ -1,14 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProjectMOFI_Server_WebAPI.MongoDB;
 
 namespace ProjectMOFI_Server_WebAPI.Controllers {
     [Route("[controller]")]
     [ApiController]
     public class AttendeeController : ControllerBase {
 
+        IWebHostEnvironment _webHostEnvironment;
+        MongoConnection _connection;
+
+        public AttendeeController(IWebHostEnvironment env, IConfiguration config) {
+            _webHostEnvironment = env;
+            _connection = new MongoConnection(config);
+        }
+
         [HttpGet]
-        public async Task<IActionResult> GetUsers() {
+        public async Task<IActionResult> GetAttendees() {
             try {
-                return await Task.FromResult(Ok("Hooray!"));
+                return await Task.FromResult(Ok(_connection.LoadRecords()));
             }
             catch (Exception ex) {
                 Console.WriteLine(ex.StackTrace);
