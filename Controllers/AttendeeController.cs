d32cf92099e8fa429d47f78edc4585d873fd5b93
kalrit718 <kalindu.rithmal.other@gmail.com>
2022-04-23 15:47:05 +0530

    Added GetAttendee method

--- Controllers/AttendeeController.cs
+++ Controllers/AttendeeController.cs
@@ -27,5 +27,22 @@ namespace ProjectMOFI_Server_WebAPI.Controllers {
             }
         }
 
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUser(string id) {
+            try {
+                if (string.IsNullOrWhiteSpace(id)) {
+                    return BadRequest();
+                }
+                return await Task.FromResult(Ok(_connection.LoadRecordById(id)));
+            }
+            catch (ArgumentException ex) {
+                return StatusCode(404, ex.Message);
+            }
+            catch (Exception ex) {
+                Console.WriteLine(ex.StackTrace);
+                return StatusCode(500, "[ERROR]- An unexpected error occured!");
+            }
+        }
+
     }
 }
